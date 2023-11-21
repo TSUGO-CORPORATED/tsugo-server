03bbe6f5fde654bbea3e6a5bff14b1ae547475b4
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-11-21 14:57:36 +0900

    feat: add controller for user

--- server.ts
+++ server.ts
@@ -18,3 +18,17 @@ app.get('/', (req: Request, res: Response) => {
 // INITIATE SERVER
 const PORT: string = process.env.PORT || "8080";
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+
+// IMPORTING DATABASE CONTROLLER
+import userController from "./src/user/user-controller";
+
+// User
+app.get('/user/:email', userController.getUser);
+  // to access: http://localhost:8080/user/abc@gmail.com
+  // this has parameter "id" that must be specified
+  // will return an object containing the user id, email, first name and last name
+  // ex: {id:31, email:"client1@gmail.com", firstName:"firstname123", lastName:"lastname123"}
+app.post('/user', userController.createUser);
+  // to access: http://localhost:8080/user
+  // body, raw, json
+  // ex: {email: "testemail", firstName: "firstname", lastName: "lastname"}
