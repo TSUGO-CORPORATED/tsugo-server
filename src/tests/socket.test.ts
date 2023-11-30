0f361d8334f54bbfdf7f79deb5cc34f5da03c723
vororo <adamolsson@outlook.com>
2023-11-30 16:23:16 +0900

    feat: set up Jest and added basic socket test example

---
+++ src/tests/socket.test.ts
@@ -0,0 +1,64 @@
+import { createServer } from "node:http";
+import { type AddressInfo } from "node:net";
+import { io as ioc, type Socket as ClientSocket } from "socket.io-client";
+import { Server, type Socket as ServerSocket } from "socket.io";
+
+function waitFor(socket: ServerSocket | ClientSocket, event: string) {
+  return new Promise((resolve) => {
+    socket.once(event, resolve);
+  });
+}
+
+describe("my awesome project", () => {
+  let io:any, serverSocket:any, clientSocket:any;
+
+  beforeAll((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = (httpServer.address() as AddressInfo).port;
+      clientSocket = ioc(`http://localhost:${port}`);
+      io.on("connection", (socket: ClientSocket) => {
+        serverSocket = socket;
+      });
+      clientSocket.on("connect", done);
+    });
+  });
+
+  afterAll(() => {
+    io.close();
+    clientSocket.disconnect();
+  });
+
+  test("should work", (done) => {
+    clientSocket.on("hello", (arg:string) => {
+      expect(arg).toBe("world");
+      done();
+    });
+    serverSocket.emit("hello", "world");
+  });
+
+  test("should work with an acknowledgement", (done) => {
+    serverSocket.on("hi", (cb:any) => {
+      cb("hola");
+    });
+    clientSocket.emit("hi", (arg:any) => {
+      expect(arg).toBe("hola");
+      done();
+    });
+  });
+
+  test("should work with emitWithAck()", async () => {
+    serverSocket.on("foo", (cb:any) => {
+      cb("bar");
+    });
+    const result = await clientSocket.emitWithAck("foo");
+    expect(result).toBe("bar");
+  });
+
+  test("should work with waitFor()", () => {
+    clientSocket.emit("baz");
+
+    return waitFor(serverSocket, "baz");
+  });
+});
