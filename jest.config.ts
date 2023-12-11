3f5f4806914d662b2463669c36a2d0900ad87b0b
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-12-11 11:57:42 +0900

    feat: try test on production

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const jestConfig: JestConfigWithTsJest = {
+  // [...]
+  // Replace `ts-jest` with the preset you want to use
+  // from the above list
+  preset: 'ts-jest',
+}
+
+export default jestConfig
+
+
+
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: 'ts-jest',
+//   testEnvironment: 'node'
+// };
